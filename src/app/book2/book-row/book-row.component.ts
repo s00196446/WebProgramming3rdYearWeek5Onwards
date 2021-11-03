5b826f5aba30339f0ce8ef8afcb67da17c40988a
Eric Leonard <S00196446@mail.itsligo.ie>
2021-11-03 17:41:34 +0000

    for own work

---
+++ src/app/book2/book-row/book-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-row',
+  templateUrl: './book-row.component.html',
+  styleUrls: ['./book-row.component.css']
+})
+export class BookRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
